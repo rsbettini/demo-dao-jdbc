45bd5bd60f37e59561b970fa524ff63d0e1f8139
Robson Bettini <robson.bettini@hotmail.com>
2022-08-25 22:03:44 -0300

    FindAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,13 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 03: Seller findAll ===");
+		list = sellerDao.findAll();
+		
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
